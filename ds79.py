19b51ac18cff6056068cd281fbf82905b3b69bd5
William Henney <will@henney.org>
2021-08-21 18:37:21 -0500

    Fix up style of library code using black

--- ds79.py
+++ ds79.py
@@ -5,31 +5,37 @@ import numpy as np
 from scipy.special import erf
 
 SQRT_PI = np.sqrt(np.pi)
-G0_CONSTANT = 3.0*SQRT_PI/8.0
+G0_CONSTANT = 3.0 * SQRT_PI / 8.0
+
+
 def _G0(s):
     "Draine & Salpeter's approximate version"
-    return (s/G0_CONSTANT)*np.sqrt(1.0 + (G0_CONSTANT*s)**2)
+    return (s / G0_CONSTANT) * np.sqrt(1.0 + (G0_CONSTANT * s) ** 2)
+
 
 def _G2byG0(s):
     "My improved approximation to the ratio"
-    return 1.0/(2.0 + s**2 + s**4)
+    return 1.0 / (2.0 + s ** 2 + s ** 4)
+
 
 # Exact versions, because why not?
 def _G0_exact(s):
     "DS79 equation 5a"
-    rslt = (s**2 + 1.0 - 1.0/(4.0*s**2))*erf(s)
-    rslt += (s + 1.0/(2*s))*np.exp(-s**2)/SQRT_PI
+    rslt = (s ** 2 + 1.0 - 1.0 / (4.0 * s ** 2)) * erf(s)
+    rslt += (s + 1.0 / (2 * s)) * np.exp(-(s ** 2)) / SQRT_PI
     return rslt
+
 
 def _G2_exact(s):
     "DS79 equation 6a"
-    return erf(s)/s**2 - 2*np.exp(-s**2)/(s*SQRT_PI)
+    return erf(s) / s ** 2 - 2 * np.exp(-(s ** 2)) / (s * SQRT_PI)
+
 
 def _ln_Lambda(n, T):
     """
     Coulomb logarithm: natural log of plasma parameter
     """
-    return 23.267 + 1.5*np.log(T/1e4) - 0.5*np.log(n)
+    return 23.267 + 1.5 * np.log(T / 1e4) - 0.5 * np.log(n)
 
 
 # Thermal speed of proton at 1e4 K
@@ -41,6 +47,7 @@ class Collider(object):
     """
     Different colliders that can exert drag force on the grains
     """
+
     def __init__(self, name, A=1.0, Z=1.0, abun=1.0):
         """
         Each collider has atomic mass `A`, charge `Z`, and abundance
@@ -50,6 +57,7 @@ class Collider(object):
         self.A = A
         self.Z = Z
         self.abun = abun
+
 
 DEFAULT_COLLIDERS = [
     Collider("proton"),
@@ -64,6 +72,8 @@ HARD_IONIZATION_COLLIDERS = DEFAULT_COLLIDERS[:-1] + [
 
 
 SMALL_NUMBER = 1e-7
+
+
 def Fdrag(w, T=1e4, phi=10.0, n=1.0, colliders=DEFAULT_COLLIDERS):
     """
     Sum of Epstein and Coulomb contributions to gas-grain drag as a
@@ -79,13 +89,14 @@ def Fdrag(w, T=1e4, phi=10.0, n=1.0, colliders=DEFAULT_COLLIDERS):
     """
     rslt = np.zeros_like(w)
     for c in colliders:
-        w0 = CHARACTERISTIC_SPEED*np.sqrt(T/1e4/c.A)
+        w0 = CHARACTERISTIC_SPEED * np.sqrt(T / 1e4 / c.A)
         s = w / w0
         # Guard against divide by zero
-        s = np.abs(s) + SMALL_NUMBER 
+        s = np.abs(s) + SMALL_NUMBER
         # Can't use += operator when w is a vector
-        rslt = rslt + c.abun * (_G0_exact(s) +
-                                _ln_Lambda(n, T)*(c.Z*phi)**2*_G2_exact(s))
+        rslt = rslt + c.abun * (
+            _G0_exact(s) + _ln_Lambda(n, T) * (c.Z * phi) ** 2 * _G2_exact(s)
+        )
     return rslt
 
 
@@ -106,11 +117,13 @@ def Fdrag_components(w, T=1e4, phi=10.0, n=1.0, colliders=DEFAULT_COLLIDERS):
     """
     rslt = {}
     for c in colliders:
-        w0 = CHARACTERISTIC_SPEED*np.sqrt(T/1e4/c.A)
+        w0 = CHARACTERISTIC_SPEED * np.sqrt(T / 1e4 / c.A)
         s = w / w0
         # return dict of 2-tuples for (Epstein, Coulomb) contributions
-        rslt[c.name] = (c.abun*_G0_exact(s),
-                        c.abun*_ln_Lambda(n, T)*(c.Z*phi)**2*_G2_exact(s))
+        rslt[c.name] = (
+            c.abun * _G0_exact(s),
+            c.abun * _ln_Lambda(n, T) * (c.Z * phi) ** 2 * _G2_exact(s),
+        )
     return rslt
 
 
@@ -126,9 +139,9 @@ def Fdrag_approx(w, T=1e4, phi=10.0):
     """
     rslt = np.zeros_like(w)
     for c in DEFAULT_COLLIDERS:
-        w0 = CHARACTERISTIC_SPEED*np.sqrt(T/1e4/c.A)
+        w0 = CHARACTERISTIC_SPEED * np.sqrt(T / 1e4 / c.A)
         s = w / w0
-        rslt = rslt + c.abun*_G0(s)*(1.0 +
-                                     APPROX_COULOMB_LAMBDA*
-                                     (c.Z*phi)**2*_G2byG0(s))
+        rslt = rslt + c.abun * _G0(s) * (
+            1.0 + APPROX_COULOMB_LAMBDA * (c.Z * phi) ** 2 * _G2byG0(s)
+        )
     return rslt
--- duststream.py
+++ duststream.py
@@ -2,13 +2,26 @@ import numpy as np
 from scipy.optimize import brentq
 import ds79
 
-MICRON = 1e-4                   # cm
+MICRON = 1e-4  # cm
+
 
 class DustStream(object):
     """A dust stream incident upon a star"""
-    def __init__(self, L4=1.0, vinf=40.0, n=1.0, a=0.02,
-                 thetaB=0.0,
-                 eta=0.01, T=1e4, kappa=600.0, Qp=2.0, rho_d=3.0, phi_norm=1.0):
+
+    def __init__(
+        self,
+        L4=1.0,
+        vinf=40.0,
+        n=1.0,
+        a=0.02,
+        thetaB=0.0,
+        eta=0.01,
+        T=1e4,
+        kappa=600.0,
+        Qp=2.0,
+        rho_d=3.0,
+        phi_norm=1.0,
+    ):
         # Star properties
         self.L4 = L4
         self.eta = eta
@@ -16,40 +29,45 @@ class DustStream(object):
         self.vinf = vinf
         self.n = n
         self.T = T
-        self.kappa = kappa            # cm^2 / g
-        self.cs = 11.4*np.sqrt(T/1e4) # km / s
-        self.taustar = 0.0089*(kappa/600.0)*np.sqrt(L4*n)*(10.0/vinf)
-        self.Rstar = 2.21*np.sqrt(L4/n)*(10.0/vinf)  # pc
+        self.kappa = kappa  # cm^2 / g
+        self.cs = 11.4 * np.sqrt(T / 1e4)  # km / s
+        self.taustar = 0.0089 * (kappa / 600.0) * np.sqrt(L4 * n) * (10.0 / vinf)
+        self.Rstar = 2.21 * np.sqrt(L4 / n) * (10.0 / vinf)  # pc
         # Dust grain properties
         self.Qp = Qp
-        self.a = a              # micron
-        self.rho_d = rho_d      # g / cm^3
-        self.kappa_d = 3.0*Qp/(4.0*a*MICRON*rho_d)
-        self.drag_constant = (4.0/self.Qp)*(self.cs*self.taustar*self.kappa_d
-                                            /(self.vinf*self.kappa))**2
+        self.a = a  # micron
+        self.rho_d = rho_d  # g / cm^3
+        self.kappa_d = 3.0 * Qp / (4.0 * a * MICRON * rho_d)
+        self.drag_constant = (4.0 / self.Qp) * (
+            self.cs * self.taustar * self.kappa_d / (self.vinf * self.kappa)
+        ) ** 2
         self.phi_norm = phi_norm
 
         # Magnetic field
-        self.thetaB = thetaB    # B-field angle from x-axis, in degrees
-        self.b_hat = np.array([
-            np.cos(np.deg2rad(thetaB)),
-            np.sin(np.deg2rad(thetaB)),
-            0.0,
-        ])
-        
-        # R**: drag-free turn-around radius
-        self.Rstarstar = 2*(self.kappa_d/self.kappa)*self.taustar*self.Rstar
-        # P_rad/P_gas at R**
-        self.Upstarstar = (self.vinf/self.cs)**2 * (self.Rstar/self.Rstarstar)**2
-        # Bow-shock radius in strong coupling limit
-        a, b = 0.0, 2*np.sqrt(1.0 + self.eta)
-        x = brentq(xfunc, a, b, args=(self.taustar, self.eta))
-        self.R0 = x*self.Rstar
+        self.thetaB = thetaB  # B-field angle from x-axis, in degrees
+        self.b_hat = np.array(
+            [
+                np.cos(np.deg2rad(thetaB)),
+                np.sin(np.deg2rad(thetaB)),
+                0.0,
+            ]
+        )
 
-        
+        # R**: drag-free turn-around radius
+        self.Rstarstar = 2 * (self.kappa_d / self.kappa) * self.taustar * self.Rstar
+        # P_rad/P_gas at R**
+        self.Upstarstar = (self.vinf / self.cs) ** 2 * (
+            self.Rstar / self.Rstarstar
+        ) ** 2
+        # Bow-shock radius in strong coupling limit
+        a, b = 0.0, 2 * np.sqrt(1.0 + self.eta)
+        x = brentq(xfunc, a, b, args=(self.taustar, self.eta))
+        self.R0 = x * self.Rstar
+
+
 def xfunc(x, ts, eta):
     """Function to be zeroed to find R_0 / R_*"""
-    return x**2 - (1.0 - np.exp(-2*ts*x)) - eta
+    return x ** 2 - (1.0 - np.exp(-2 * ts * x)) - eta
 
 
 def dydt_1d(y, t, s):
@@ -61,7 +79,7 @@ def dydt_1d(y, t, s):
     """
     x, u = y
     dxdt = u
-    w = (u + 1.0)*s.vinf
+    w = (u + 1.0) * s.vinf
     dudt = total_accel(x, w, s)
     return [dxdt, dudt]
 
@@ -74,14 +92,13 @@ def total_accel(x, w, s):
     turnaround radius).  `w` is relative velocity in km/s.  `s` is a
     `DustStream` object.
     """
-    return 0.5*(x**(-2)
-                - s.drag_constant*ds79.Fdrag(w, s.T, phi(x, s), s.n))
+    return 0.5 * (x ** (-2) - s.drag_constant * ds79.Fdrag(w, s.T, phi(x, s), s.n))
 
 
 def dydt_2d(state, t, s):
     """
     2D equation of motion for dust grain, subject to radiation force
-    and drag force. Used in ODE evolution of `state` vector with `t` 
+    and drag force. Used in ODE evolution of `state` vector with `t`
 
     Extra argument `s` is a DustStream() instance
     """
@@ -111,20 +128,20 @@ def vector_accel_2d(x, y, u, v, s, ugas=-1.0, vgas=0.0):
     w = np.hypot(wx, wy)
 
     # Magnitude of radiative acceleration
-    a_rad = 0.5/r**2
-    # Magnitude of drag 
-    a_drag = 0.5*s.drag_constant*ds79.Fdrag(w*s.vinf, s.T, phi(r, s))
+    a_rad = 0.5 / r ** 2
+    # Magnitude of drag
+    a_drag = 0.5 * s.drag_constant * ds79.Fdrag(w * s.vinf, s.T, phi(r, s))
     # a_rad is directed along +r_hat ...
-    ax = a_rad*x/r
-    ay = a_rad*y/r
+    ax = a_rad * x / r
+    ay = a_rad * y / r
     if w > 0.0:
         # ... while a_drag is along -w_hat
-        ax += -a_drag*wx/w
-        ay += -a_drag*wy/w
+        ax += -a_drag * wx / w
+        ay += -a_drag * wy / w
     return ax, ay
 
-def vector_accel_3d_frozen(x, y, z, vx, vy, vz, s,
-                           vxgas=-1.0, vygas=0.0, vzgas=0.0):
+
+def vector_accel_3d_frozen(x, y, z, vx, vy, vz, s, vxgas=-1.0, vygas=0.0, vzgas=0.0):
     """
     Total 3d vector acceleration on a grain: radiative + drag,
     assuming that Lorentz force is so strong that grains can only be
@@ -141,30 +158,30 @@ def vector_accel_3d_frozen(x, y, z, vx, vy, vz, s,
     acceleration
     """
     # Radius from star
-    r = np.sqrt(x**2 + y**2 + z**2)
+    r = np.sqrt(x ** 2 + y ** 2 + z ** 2)
     # Unit vector in radial direction
-    r_hat = np.array([x/r, y/r, z/r])
+    r_hat = np.array([x / r, y / r, z / r])
     # Magnitude of radiative acceleration
-    a_rad = 0.5/r**2
-    # Contribution of radiation to acceleration vector 
-    a_vec = a_rad*r_hat
+    a_rad = 0.5 / r ** 2
+    # Contribution of radiation to acceleration vector
+    a_vec = a_rad * r_hat
 
     # Relative velocity: components and magnitude
     wx = vx - vxgas
     wy = vy - vygas
     wz = vz - vzgas
-    w = np.sqrt(wx**2 + wy**2 + wz**2)
+    w = np.sqrt(wx ** 2 + wy ** 2 + wz ** 2)
     if w > 0.0:
         # Unit vector along slip velocity
-        w_hat = np.array([wx/w, wy/w, wz/w])
-        # Magnitude of drag 
-        a_drag = 0.5*s.drag_constant*ds79.Fdrag(w*s.vinf, s.T, phi(r, s))
+        w_hat = np.array([wx / w, wy / w, wz / w])
+        # Magnitude of drag
+        a_drag = 0.5 * s.drag_constant * ds79.Fdrag(w * s.vinf, s.T, phi(r, s))
         # Contribution of drag to acceleration vector
-        a_vec += -a_drag*w_hat
+        a_vec += -a_drag * w_hat
 
     # Only retain component projected along B-field
     a_proj = np.dot(a_vec, s.b_hat)
-    ax, ay, az = a_proj*s.b_hat
+    ax, ay, az = a_proj * s.b_hat
     return ax, ay, az
 
 
@@ -184,7 +201,6 @@ def dydt_3d(state, t, s, accel_func=vector_accel_3d_frozen):
     return [dxdt, dvxdt, dydt, dvydt, dzdt, dvzdt]
 
 
-
 def phi(x, s):
     """
     Grain potential: phi = U / k T
@@ -193,7 +209,5 @@ def phi(x, s):
     turnaround radius).  `s` is a `DustStream` object.
     """
     # P_rad / P_gas
-    Up = s.Upstarstar / x**2
-    return 1.5*s.phi_norm*(np.log(Up) + 2.3)
-
-
+    Up = s.Upstarstar / x ** 2
+    return 1.5 * s.phi_norm * (np.log(Up) + 2.3)
